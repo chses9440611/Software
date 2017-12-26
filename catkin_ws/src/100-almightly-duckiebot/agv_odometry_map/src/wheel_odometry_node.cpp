284abd719b715d35a8b893478bc4b2c1c240c07a
tony <tonycar12002@gmail.com>
2017-12-26 14:01:31 +0800

    tf

--- catkin_ws/src/100-almightly-duckiebot/agv_odometry_map/src/wheel_odometry_node.cpp
+++ catkin_ws/src/100-almightly-duckiebot/agv_odometry_map/src/wheel_odometry_node.cpp
@@ -3,26 +3,33 @@
 #include <nav_msgs/Odometry.h>
 #include <math.h>
 #include <tf/transform_broadcaster.h>
-
+#include <tf/transform_listener.h>
 #include "duckietown_msgs/WheelsCmdStamped.h"
 struct Point
 {
 	float x;
 	float y;
 };
-struct Position
-{
-	Point left;
-	Point right;
-	Point mid;
-};
-Position wheel_pos;
+Point wheel_pos;
+tf::TransformListener listener;
 float agv_width = 50; //cm
+std::String bot_name;
+std::String node_name;
+ros::Publisher pub_odom;
+void tfWheelOdometryListner()
+{
+	tf::Transform transform;
+	ros::Time now = ros::Time::now();
+	try{
+		listener.waitForTransform("/world", bot_name,
+	                              now, ros::Duration(3.0));
+	    listener.lookupTransform("/world", bot_name,
+	                             now, transform);
+	}
+	
 
-ros::Publisher pub_odom ;
-ros::Publisher pub_tf;
-
-void drawWheelOdometry(const duckietown_msgs::WheelsCmdStamped::ConstPtr msg)
+}
+void tfWheelOdometrySender(const duckietown_msgs::WheelsCmdStamped::ConstPtr msg)
 {
 	//convert pulse to length(m)
 	tf::TransformBroadcaster br;
@@ -41,28 +48,28 @@ void drawWheelOdometry(const duckietown_msgs::WheelsCmdStamped::ConstPtr msg)
   	q.setRPY(0 ,0, theta);
   	transform.setRotation(q);
 
-  	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "agv"));
-	
+  	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", bot_name));
+
+	tfWheelOdometryListner();
 }
 void cbWheelCmd(const duckietown_msgs::WheelsCmdStamped::ConstPtr& msg)
 {
-	drawWheelOdometry(msg);
+	tfWheelOdometrySender(msg);
 }
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "wheel_odometry_node");
-
-	wheel_pos.left.x = 0;
-	wheel_pos.left.y = 0;
-	wheel_pos.right.x = 0;
-	wheel_pos.right.y = 0;
-	wheel_pos.mid.x = 0;
-	wheel_pos.mid.y = 0;
+	bot_name = ros::param:get_param("veh_name", bot_name);
+	node_name = ros::get_name();
+	wheel_pos.x = 0;
+	wheel_pos.y = 0;
 
 	ros::NodeHandle nh("~");
 	
 	ros::Subscriber sub_wheel_cmd = nh.subscribe("wheel_cmd", 1000, cbWheelCmd);
 	pub_odom = nh.advertise<nav_msgs::Odometry>("wheel_odometry", 1000);
+
+	while ()
 	ros::spin();
 
 	return 0;
