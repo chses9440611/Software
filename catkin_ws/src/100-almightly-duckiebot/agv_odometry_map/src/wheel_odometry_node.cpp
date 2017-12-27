5bf1833ca5c68cf189105268a3ce86696bb01382
tony <tonycar12002@gmail.com>
2017-12-27 16:26:08 +0800

    wheel odo,

--- catkin_ws/src/100-almightly-duckiebot/agv_odometry_map/src/wheel_odometry_node.cpp
+++ catkin_ws/src/100-almightly-duckiebot/agv_odometry_map/src/wheel_odometry_node.cpp
@@ -5,71 +5,106 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include "duckietown_msgs/WheelsCmdStamped.h"
+#include <sensor_msgs/LaserScan.h>
+using namespace std;
+#define WIDTH 0.5 // (m)
+#define WHEEL_RADIUS 0.085
 struct Point
 {
 	float x;
 	float y;
+	float th;
+	ros::Time time;
 };
-Point wheel_pos;
-tf::TransformListener listener;
-float agv_width = 50; //cm
-std::String bot_name;
-std::String node_name;
-ros::Publisher pub_odom;
-void tfWheelOdometryListner()
-{
-	tf::Transform transform;
-	ros::Time now = ros::Time::now();
-	try{
-		listener.waitForTransform("/world", bot_name,
-	                              now, ros::Duration(3.0));
-	    listener.lookupTransform("/world", bot_name,
-	                             now, transform);
-	}
-	
 
+Point wheel_pos, wheel_pos_record;
+std::string bot_name;
+std::string node_name;
+ros::Publisher pub_odom;
+tf::TransformBroadcaster* br;
+
+void drawWheelOdom()
+{
+	nav_msgs::Odometry odom;
+	odom.header.stamp = ros::Time::now();
+	odom.header.frame_id = "map";
+	odom.child_frame_id = "odom_frame";
+
+	odom.pose.pose.position.x = wheel_pos.x;
+  	odom.pose.pose.position.y = wheel_pos.y;
+  	odom.pose.pose.position.z = 0.0;
+  	odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(wheel_pos.th);
+
+  	//cal speed 
+  	float dt = wheel_pos.time.toSec() - wheel_pos_record.time.toSec();
+    odom.twist.twist.linear.x = (wheel_pos_record.x - wheel_pos.x) * 1/dt;
+    odom.twist.twist.linear.y = (wheel_pos_record.y - wheel_pos.y) * 1/dt;
+    odom.twist.twist.angular.z = (wheel_pos_record.th - wheel_pos.th) * 1/dt;
+
+  	pub_odom.publish(odom);
 }
-void tfWheelOdometrySender(const duckietown_msgs::WheelsCmdStamped::ConstPtr msg)
+void tfWheelOdomSender(const duckietown_msgs::WheelsCmdStamped::ConstPtr msg)
 {
 	//convert pulse to length(m)
-	tf::TransformBroadcaster br;
+	float dis_L = msg->vel_left / 4096 * WHEEL_RADIUS * 2.0 * 3.14 ;
+	float dis_R = msg->vel_right / 4096 * WHEEL_RADIUS * 2.0 * 3.14 ;
+
+	wheel_pos_record = wheel_pos ;
+	// update wheel pose
+	wheel_pos.th += (1/(WIDTH))*(dis_L-dis_R);
+	wheel_pos.x += 0.5*(cos(wheel_pos.th)*dis_R+cos(wheel_pos.th)*dis_L);
+	wheel_pos.y += 0.5*(sin(wheel_pos.th)*dis_R+sin(wheel_pos.th)*dis_L);
+	wheel_pos.time = ros::Time::now();
+
+	//cout << wheel_pos.x << " " << wheel_pos.y << " " << wheel_pos.th << "" << endl;
+
+	// send transform
 	tf::Transform transform;
 	tf::Quaternion q;
-	float left = msg->vel_left / 4096 * 8.5 * 2 * 3.14 / 100;
-	float right = msg->vel_right / 4096 * 8.5 * 2 * 3.14 / 100;
-	float r = (left < right) ? (-0.5 * left / right) : (0.5 * right / left);
-	float theta = left / r ; //(rad)
-	if (r == 1)
-	{
-		theta = 0;
-	}
-	
-	transform.setOrigin(tf::Vector3(wheel_pos.mid.x, wheel_pos.mid.y, 0));
-  	q.setRPY(0 ,0, theta);
+	transform.setOrigin(tf::Vector3(wheel_pos.x, wheel_pos.y, 0));
+  	q.setRPY(0 ,0, wheel_pos.th);
   	transform.setRotation(q);
+  	try
+  	{
+  		br->sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "odom_frame"));
+  	}
+  	catch(tf::TransformException &ex)
+  	{
+		ROS_ERROR("transfrom exception : %s",ex.what());
+  	}
 
-  	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", bot_name));
+	drawWheelOdom();
 
-	tfWheelOdometryListner();
 }
 void cbWheelCmd(const duckietown_msgs::WheelsCmdStamped::ConstPtr& msg)
 {
-	tfWheelOdometrySender(msg);
+	tfWheelOdomSender(msg);
+}
+void cbLaserScan(const sensor_msgs::LaserScanStamped:;ConstPtr& msg)
+{
+
+
 }
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "wheel_odometry_node");
-	bot_name = ros::param:get_param("veh_name", bot_name);
-	node_name = ros::get_name();
+	ros::NodeHandle nh("~");
+	tf::TransformBroadcaster br_instance;
+	br = &br_instance;
+
+	//Initialize
 	wheel_pos.x = 0;
 	wheel_pos.y = 0;
+	wheel_pos.th = 0;
+	wheel_pos_record = wheel_pos ;
 
-	ros::NodeHandle nh("~");
-	
+	nh.getParam("veh_name", bot_name);
+	node_name = ros::this_node::getName();
+
 	ros::Subscriber sub_wheel_cmd = nh.subscribe("wheel_cmd", 1000, cbWheelCmd);
+	ros::Subscriber sub_laser_scan = nh.subscribe("/scan", 1000, cbLaserScan);
 	pub_odom = nh.advertise<nav_msgs::Odometry>("wheel_odometry", 1000);
 
-	while ()
 	ros::spin();
 
 	return 0;
