ee7f5e17a5074b28ce1496cc8e53b590da1f0771
tony <tonycar12002@gmail.com>
2018-01-04 21:47:08 +0800

    add pcl viewer

--- catkin_ws/src/100-almightly-duckiebot/agv_odometry_map/src/pcl_odometry_node.cpp
+++ catkin_ws/src/100-almightly-duckiebot/agv_odometry_map/src/pcl_odometry_node.cpp
@@ -1,58 +1,179 @@
 #include <iostream>
 #include <ros/ros.h>
 #include <math.h>
+#include <boost/thread/thread.hpp>
+#include <tf_conversions/tf_eigen.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <ros/console.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/keypoints/sift_keypoint.h>
-#include <pcl/filters/statistical_outlier_removal.h>
+#include <boost/thread/thread.hpp>
+#include <pcl/common/common_headers.h>
+#include <pcl/common/transforms.h>  
+#include <pcl/console/parse.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/keypoints/sift_keypoint.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
 #include "pcl_ros/point_cloud.h"
 #include <sensor_msgs/PointCloud2.h>
 #include <nav_msgs/Odometry.h>
+#include <ctime>
+#include <boost/thread/thread.hpp>
 using namespace std;
 std::string name;
-ros::Publisher pub_cloud;                                                       
+ros::Publisher pub_cloud; 
+ros::Publisher pub_cloud_test;                                                       
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_feature_record(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_feature(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_map(new pcl::PointCloud<pcl::PointXYZ>);
+tf::TransformBroadcaster* br;
+Eigen::Matrix4f matrix_record;
 
-void cbPointCloud(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud_msg)
+boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+int counter = 0;
+bool first_Set_Viewer = true;
+
+tf::Transform transformEigenToTF(Eigen::Matrix4f tm)
 {
-	pcl::PointCloud<pcl::PointNormal>::Ptr cloud_normals(new pcl::PointCloud<pcl::PointNormal>);
-	/*
-	// normal of PCL
-	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree_xyz(new pcl::search::KdTree<pcl::PointXYZ>);
-	pcl::NormalEstimation<pcl::PointXYZ, pcl::PointNormal> ne;
-	ne.setInputCloud(cloud_outlier);
-	ne.setSearchMethod(tree_xyz);
-	ne.setRadiusSearch(0.2);
-	ne.compute(*cloud_normals);
-	for(int i = 0; i<cloud_normals->points.size(); ++i)
+	tf::Vector3 origin;
+	// transform to double type
+  	origin.setValue(static_cast<double>(tm(0,3)),static_cast<double>(tm(1,3)),static_cast<double>(tm(2,3)));
+
+  	tf::Matrix3x3 tf3d;
+  	tf3d.setValue(static_cast<double>(tm(0,0)), static_cast<double>(tm(0,1)), static_cast<double>(tm(0,2)), 
+        static_cast<double>(tm(1,0)), static_cast<double>(tm(1,1)), static_cast<double>(tm(1,2)), 
+        static_cast<double>(tm(2,0)), static_cast<double>(tm(2,1)), static_cast<double>(tm(2,2)));
+
+  	tf::Quaternion tfqt;
+  	tf3d.getRotation(tfqt);
+
+  	tf::Transform trans;
+  	trans.setOrigin(origin);
+  	trans.setRotation(tfqt);
+	return trans;
+}
+tf::Transform icpWithPointXYZ(pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_source, 
+	pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_target)
+{
+	clock_t begin = clock();
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_icp(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_test(new pcl::PointCloud<pcl::PointXYZ>);
+
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree_1(new pcl::search::KdTree<pcl::PointXYZ>);
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree_2(new pcl::search::KdTree<pcl::PointXYZ>);
+	tree_1->setInputCloud(cloud_source);
+	tree_2->setInputCloud(cloud_target);
+	pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+	icp.setInputCloud(cloud_source);
+	icp.setInputTarget(cloud_target);
+	icp.setMaxCorrespondenceDistance(1500); 
+	icp.setTransformationEpsilon(1e-10);
+	icp.setEuclideanFitnessEpsilon(0.01);
+	icp.setMaximumIterations(500);
+	icp.align(*cloud_icp);
+
+  	clock_t end = clock();
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	cout<<"icp compute time = " << elapsed_secs<<endl;
+
+	Eigen::Matrix4f trans_eigen = icp.getFinalTransformation() ;
+	pcl::transformPointCloud(*cloud_source, *cloud_test, trans_eigen);
+	matrix_record = matrix_record * trans_eigen;
+	tf::Transform trans = transformEigenToTF(matrix_record);
+
+
+	pub_cloud_test.publish(*cloud_test);
+	pub_cloud.publish(*cloud_target);
+	return trans;
+
+}
+
+void cbPointCloud(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg)
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+ 	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+
+	pcl::fromROSMsg (*cloud_msg, *cloud);
+	pcl::copyPointCloud(*cloud,*cloud_feature);
+	if(counter == 6)
 	{
-		cloud_normals->points[i].x = cloud_voxel->points[i].x;
-		cloud_normals->points[i].y = cloud_voxel->points[i].y;
-		cloud_normals->points[i].z = cloud_voxel->points[i].z;
-	}*/
-	
-	/*
-	// Parameters for sift computation
-	pcl::search::KdTree<pcl::PointNormal>::Ptr tree_sift(new pcl::search::KdTree<pcl::PointNormal>);
-	const float min_scale = 0.01f;
-	const int n_octaves = 3;
-	const int n_scales_per_octave = 4;
-	const float min_contrast = 0.001f;
-	//Sift 
-	pcl::SIFTKeypoint<pcl::PointNormal, pcl::PointXYZ> sift;
-	sift.setInputCloud(cloud_normals);
-	sift.setSearchMethod(tree_sift);
-	sift.setScales(min_scale, n_octaves, n_scales_per_octave);
-	sift.setMinimumContrast(min_contrast);
-	sift.compute(*cloud_sift);
-	*/
+		counter = 0;
 
-	pub_cloud.publish(*cloud_msg);
+		// normal of PCL -> feature
+		pcl::search::KdTree<pcl::PointXYZ>::Ptr tree_xyz(new pcl::search::KdTree<pcl::PointXYZ>);
+		pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+		ne.setInputCloud(cloud);
+		ne.setSearchMethod(tree_xyz);
+		ne.setRadiusSearch(0.2);
+		ne.compute(*cloud_normals);
+		/*for (int i = 0; i< cloud_normals->points.size(); i++)
+		{
+			cloud_normals->points[i].x = cloud_feature->points[i].x;
+			cloud_normals->points[i].y = cloud_feature->points[i].y;
+			cloud_normals->points[i].z = cloud_feature->points[i].z;
+		}*/
 
+
+		/*
+		// Parameters for sift computation
+		pcl::search::KdTree<pcl::PointNormal>::Ptr tree_sift(new pcl::search::KdTree<pcl::PointNormal>);
+		const float min_scale = 0.01f;
+		const int n_octaves = 3;
+		const int n_scales_per_octave = 4;
+		const float min_contrast = 0.001f;
+		//Sift 
+		pcl::SIFTKeypoint<pcl::PointNormal, pcl::PointXYZ> sift;
+		sift.setInputCloud(cloud_feature);
+		sift.setSearchMethod(tree_sift);
+		sift.setScales(min_scale, n_octaves, n_scales_per_octave);
+		sift.setMinimumContrast(min_contrast);
+		sift.compute(*cloud_sift);
+		*/
+
+		//ICP
+		if (cloud_feature_record->points.size() > 0 )
+		{
+			tf::Transform trans = icpWithPointXYZ(cloud_feature, cloud_feature_record);
+ 		  	try
+		  	{
+		  		br->sendTransform(tf::StampedTransform(trans, cloud_msg->header.stamp, "odom_frame", "base_link_3Dlidar"));
+		  	}
+		  	catch(tf::TransformException &ex)
+		  	{
+				ROS_ERROR("transfrom exception : %s",ex.what());
+		  	}
+
+		}
+		//Visualization
+		if (first_Set_Viewer)
+		{
+			first_Set_Viewer = false;
+			viewer->addPointCloud<pcl::PointXYZ> (cloud_feature, "cloud");
+			viewer->addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloud_feature, cloud_normals, 10, 0.5, "cloud_normals");
+			viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "cloud");
+			viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "cloud_normals");
+			viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1, 1, 1, "cloud");
+			viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1, 0, 0, "cloud_normals");
+
+		}
+		viewer->updatePointCloud(cloud_feature, "cloud");
+		viewer->removePointCloud ("cloud_normals");
+		viewer->addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloud_feature, cloud_normals, 10, 2, "cloud_normals");
+		viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_COLOR, 1, 0, 0, "cloud_normals");
+		pcl::copyPointCloud(*cloud_feature,*cloud_feature_record);
+
+	}
+	else
+		counter++;
+}
+
+void cbViewer(const ros::TimerEvent& event)
+{
+	viewer->spinOnce();
 }
 int main(int argc, char** argv)
 {
@@ -61,10 +182,30 @@ int main(int argc, char** argv)
 	ROS_INFO("[%s] Initializing ", name.c_str());
 
 	ros::NodeHandle nh("~");
+
+	tf::TransformBroadcaster br_instance;
+	br = &br_instance;
+
+	for (int x = 0;x<4;x++)
+	{
+		for (int y=0;y<4;y++)
+		{
+			if (x == y)
+				matrix_record(x, y) = 1;
+			else
+				matrix_record(x, y) = 0;
+		}
+	}
+
 	ros::Subscriber sub_velodyne_pcl = nh.subscribe("cloud_preprocess", 1000, cbPointCloud);
-
 	pub_cloud = nh.advertise< pcl::PointCloud<pcl::PointXYZ> >("cloud_out", 1000);
+	pub_cloud_test = nh.advertise< pcl::PointCloud<pcl::PointXYZ> >("cloud_out_test", 1000);
 
+	viewer->setBackgroundColor (0, 0, 0);
+	viewer->addCoordinateSystem (1.0);
+	viewer->initCameraParameters ();
+	ros::Timer timer = nh.createTimer(ros::Duration(0.1), cbViewer);
+	
 	ros::spin();
 
 	return 0;
--- catkin_ws/src/100-almightly-duckiebot/agv_odometry_map/src/pcl_preprocess_node.cpp
+++ catkin_ws/src/100-almightly-duckiebot/agv_odometry_map/src/pcl_preprocess_node.cpp
@@ -9,30 +9,56 @@
 using namespace std;
 std::string name;
 ros::Publisher pub_cloud;                                                       
-
+int counter = 0;
 void cbPointCloud(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg)
 {
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_voxel(new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_outlier(new pcl::PointCloud<pcl::PointXYZ>);
-	
-	// for ros publish PCL
-	pcl::fromROSMsg (*cloud_msg, *cloud);
+	if(counter != 3)
+	{
+		counter++;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_voxel(new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_outlier(new pcl::PointCloud<pcl::PointXYZ>);
+		
 
-	// Voxel Filter to DownSampling
-	pcl::VoxelGrid<pcl::PointXYZ> vg;
-	vg.setInputCloud(cloud);
-	vg.setLeafSize (0.05f, 0.05f, 0.05f);
-	vg.filter(*cloud_voxel);
+		// for ros publish PCL
+		pcl::fromROSMsg (*cloud_msg, *cloud);
 
-	// filter outlier (far range)
-	pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-	sor.setInputCloud(cloud_voxel);
-	sor.setMeanK(150);
-	sor.setStddevMulThresh(0.8); 
-	sor.filter(*cloud_outlier); 
+		// Voxel Filter to DownSampling
+		pcl::VoxelGrid<pcl::PointXYZ> vg;
+		vg.setInputCloud(cloud);
+		vg.setLeafSize (0.05f, 0.05f, 0.05f);
+		vg.filter(*cloud_voxel);
+		float max_range = 10, dis;
+		for (int i=0 ; i <  cloud_voxel->points.size() ; i++)
+		{
+			dis = cloud_voxel->points[i].z * cloud_voxel->points[i].z + 
+				cloud_voxel->points[i].x * cloud_voxel->points[i].x +
+				cloud_voxel->points[i].y * cloud_voxel->points[i].y;
+			dis = sqrt(dis);
 
-	pub_cloud.publish(*cloud_outlier);
+			if (cloud_voxel->points[i].z < 0.05 || dis > max_range)
+			{
+				cloud_voxel->points.erase(cloud_voxel->points.begin()+i);
+				cloud_voxel->width -=1 ;
+				cloud_voxel->points.resize(cloud_voxel->width);
+				i--;
+			}
+		}
+
+		// filter outlier 
+		/*
+		pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+		sor.setInputCloud(cloud_voxel);
+		sor.setMeanK(150);
+		sor.setStddevMulThresh(0.8); 
+		sor.filter(*cloud_outlier); */
+
+		pub_cloud.publish(*cloud_voxel);
+	}
+	else
+	{
+		counter = 0;
+	}
 
 }
 int main(int argc, char** argv)
@@ -40,7 +66,7 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "pcl_preprocess_node");
 	name = ros::this_node::getName();
 	ROS_INFO("[%s] Initializing ", name.c_str());
-
+	counter = 0;
 	ros::NodeHandle nh("~");
 	ros::Subscriber sub_velodyne_pcl = nh.subscribe("/velodyne_points", 1000, cbPointCloud);
 
